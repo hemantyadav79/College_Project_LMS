609308c029ac147fd856f4df412ceae4a2f64ebf
Hemant Kumar <113499686+hemantyadav79@users.noreply.github.com>
2025-04-01 21:51:17 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import tailwindcss-animate from "tailwindcss-animate";
 export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
